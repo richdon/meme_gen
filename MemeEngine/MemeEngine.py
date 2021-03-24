93895fb747745b38ab2fbb629037f81c5183c87a
Richard Donald <45852663+richdon@users.noreply.github.com>
2021-03-24 16:33:32 -0400

    Add files via upload

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -6,52 +6,54 @@ Created on Wed Mar 17 21:13:59 2021
 """
 from PIL import Image, ImageDraw, ImageFont
 import random
-import pathlib
-
-# Globals for img path
-here = pathlib.Path('.')
-tmp_dir = here.absolute() / 'tmp'
 
 
-class MemeEngine():
+class MemeEngine():        
+    
+    def __init__(self, save_path):
         
-    @classmethod
-    def resize(cls, path, size):
+        self.save_path = save_path
+        
+
+    def resize(self, path:str, size:int) -> str:
+        """Resize the image in path dependent on width entered
+        in size variable"""
         
         try:
             im = Image.open(path)
+            
         except FileNotFoundError:  
             print('The file was not opened')
         
         im.thumbnail(size)
-        im.save(f"{tmp_dir}/resized.jpg")       
+        im.save(f"{self.save_path}/resized.jpg")       
     
-        return f"{tmp_dir}/resized.jpg"
+        return f"{self.save_path}/resized.jpg"
     
-    @classmethod
-    def write_text(cls, path, body, author):
+    
+    def write_text(self, path, body, author):
         
         tmp = random.randint(2000, 3000)
         
         try:
-            im1 = Image.open(f"{tmp_dir}/resized.jpg")
+            im1 = Image.open(f"{self.save_path}/resized.jpg")
         except FileNotFoundError:  
             print('The file was not opened')
         
         draw = ImageDraw.Draw(im1)
         font = ImageFont.truetype("arial.ttf", 18)
         phrase = body +' - ' + author
-        draw.text((60, 350), phrase, font = font, fill = 'white')
-        im1.save(f"{tmp_dir}/{tmp}.jpg")
+        draw.text((0, 0), phrase, font = font, fill = 'white', align='center')
+        im1.save(f"{self.save_path}/{tmp}.jpg")
         
-        return f"{tmp_dir}\{tmp}.jpg"
+        return f"{self.save_path}/{tmp}.jpg"
         
-    @classmethod
-    def MakeMeme(cls, path, body, author, width = 500):
+    
+    def make_meme(self, path, body, author, width = 500):
         
         height = width
         size = (width, height)
         
-        meme_path = cls.write_text(cls.resize(path, size), body, author)
+        meme_path = self.write_text(self.resize(path, size), body, author)
         
         return meme_path
