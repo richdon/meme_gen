d729c0f24fa30bd46b097f7864c275542542ff7a
Richard Donald <45852663+richdon@users.noreply.github.com>
2021-03-26 17:44:48 -0400

    Add files via upload

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -9,8 +9,10 @@ import random
 
 
 class MemeEngine():        
+    """class to build a meme"""
     
     def __init__(self, save_path):
+        """Initialize with an output path"""
         
         self.save_path = save_path
         
@@ -32,24 +34,34 @@ class MemeEngine():
     
     
     def write_text(self, path, body, author):
+        """Open the resized image and write text on the image"""
         
         tmp = random.randint(2000, 3000)
+        im = Image.open(f"{self.save_path}/resized.jpg")
+        x, y = 10, 10
+        pointsize = 30
+        fillcolor = 'white'
+        shadowcolor = 'black'        
         
-        try:
-            im1 = Image.open(f"{self.save_path}/resized.jpg")
-        except FileNotFoundError:  
-            print('The file was not opened')
         
-        draw = ImageDraw.Draw(im1)
-        font = ImageFont.truetype("arial.ttf", 18)
-        phrase = body +' - ' + author
-        draw.text((0, 0), phrase, font = font, fill = 'white', align='center')
-        im1.save(f"{self.save_path}/{tmp}.jpg")
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype("impact.ttf", pointsize)
+        text = body +' - ' + author
+        
+        # thicker border
+        draw.text((x-1, y-1), text, font=font, fill=shadowcolor)
+        draw.text((x+1, y-1), text, font=font, fill=shadowcolor)
+        draw.text((x-1, y+1), text, font=font, fill=shadowcolor)
+        draw.text((x+1, y+1), text, font=font, fill=shadowcolor)
+        
+        draw.text((x, y), text, font = font, fill = fillcolor, align='center')
+        im.save(f"{self.save_path}/{tmp}.jpg")
         
         return f"{self.save_path}/{tmp}.jpg"
         
     
     def make_meme(self, path, body, author, width = 500):
+        """Build a meme using the helper methods"""
         
         height = width
         size = (width, height)
